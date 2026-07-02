e1111b791abf34b5283e59224aa6f49f6120ba0d
Elo <197076178+elo-tidy@users.noreply.github.com>
2026-07-02 15:57:23 +0200

    fix: round Stripe order amount correctly in cents

--- shared/utils/maths.ts
+++ shared/utils/maths.ts
@@ -1,9 +1,11 @@
-export const numberWithTwoDecimals = (price: number): string => price.toFixed(2)
+export const numberWithTwoDecimals = (price: number): string =>
+  price.toFixed(2);
 
 export const formatPriceWithTwoDecimals = (price: number): string =>
-  numberWithTwoDecimals(Math.floor(price * 100) / 100)
+  numberWithTwoDecimals(Math.floor(price * 100) / 100);
 
 export const priceFromCentsToEuros = (price: number): string =>
-  formatPriceWithTwoDecimals(price / 100)
+  formatPriceWithTwoDecimals(price / 100);
 
-export const priceFromEurosToCents = (price: number): number => Math.floor(price * 100)
+export const priceFromEurosToCents = (price: number): number =>
+  Math.round(price * 100);
