27fa9731d76bdddfa6751a043e2643136f0dea65
Elo <197076178+elo-tidy@users.noreply.github.com>
2026-08-24 12:23:35 +0200

    fix(shared): update shared package config

---
+++ shared/index.ts
@@ -0,0 +1,16 @@
+export * from "./types/Product";
+export * from "./types/Categories";
+export * from "./types/Order";
+export * from "./types/User";
+export * from "./types/Cart";
+export * from "./types/ShippingMode";
+export * from "./types/PaymentStatus";
+export * from "./types/stripe";
+export * from "./types/database";
+
+export * from "./utils/maths";
+export * from "./utils/Cart";
+
+export * from "./services/SupabaseServices";
+
+export * from "./composables/useDeliveryEstimation";
